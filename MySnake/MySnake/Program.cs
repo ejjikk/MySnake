240679b7babb9c4bd976d0ca1f83577bba293d35
ejjikk <aleksina.katya@gmail.com>
2016-02-26 15:52:45 +0300

    Draw

--- MySnake/MySnake/Program.cs
+++ MySnake/MySnake/Program.cs
@@ -10,6 +10,19 @@ namespace MySnake
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
+=======
+            Point p1 = new Point();
+            p1.Draw(3, 4, '*');
+
+            Console.ReadLine();
+        }
+
+        static void Draw (int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+>>>>>>> Stashed changes
         }
     }
 }
